5b184066ae040324e93898745fc2d42ccb9809c4
TomReticky <tomas.reticky@gmail.com>
2025-04-28 15:49:05 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Quantity, Length, Width, Thickness, cost, weight [milimeters, grams]
+sheet_x = [3, 1500, 148.4, 0.8, 500, 483]
+sheet_y = [4, 1500, 40, 0.8, 300, 128]
+stringer = [8, 1500, 20, 1.5, 150, 237]
+
+# Quantity, cost, weight [milimeters, grams]
+bolt_nut = [60, 50, 2.33]
+
+sigma_ult = 220 # [Mpa]
+sigma_y = 150 # [Mpa]
+E = 69700 # [Mpa]
+rho = 2680 # [kg/m^3]
